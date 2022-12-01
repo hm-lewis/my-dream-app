6f59aceb8d6a62cf2d4f5d384f7116f4e31d54e7
Lewis Heidi <heidimaria.lewis@gmail.com>
2022-12-01 18:19:52 +0200

    Add class to server and servers selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `
-  <section>
-    <h1>I'm in the Servers Component</h1>
-    <p>Below this I have 2 Server Components</p>
-    <app-server></app-server>
-    <app-server></app-server>
-  </section>
-  `,
+ // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
+  templateUrl:'./servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
@@ -20,3 +15,13 @@ export class ServersComponent implements OnInit {
   }
 
 }
+
+// Tehtävä 37. Working with component templates:
+  // template: `
+  // <section>
+  //   <h1>I'm in the Servers Component</h1>
+  //   <p>Below this I have 2 Server Components</p>
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // </section>
+  // `,
